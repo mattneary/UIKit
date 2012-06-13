077f908298a1972d36ccc2bc8ec3f6604a017495
Matt Neary <neary.matt@gmail.com>
2012-06-13 17:28:01 -0400

    instance methods read from this

--- UIView.js
+++ UIView.js
@@ -5,18 +5,28 @@ define(["./Foundation"], function($ ) {
 		$.extend(this, {
 			view: $.Element("div", {
 				class: "UIView"						
-			})
+			}), initialized: false
 		});
 	};			
 	UIView.prototype = {				
 		appendToView: function(targetView) {
 			var view = this.view;
-			targetView.appendChild(view);
+			this.init();
+			targetView.appendChild(view);			
 			return this;
 		}, sub: function(cb) {
 			cb.call(this);
 			return this;
-		}
+		}, _init: function() {
+			if( !this.initialized == true ) {
+				console.log("init!");
+				if( this.delegate && this.delegate.viewWillLoad)
+					this.view = this.delegate.viewWillLoad(this.view) || this.view;
+				initialized = true;
+			}
+		}, init: function() {
+			this._init();
+		}, delegate: null
 	};	
 	//define self
 	return UIView;
